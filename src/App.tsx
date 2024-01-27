f84214af7704c6752d451adac41bd462fb9c0109
jmgarcia0310 <garcia.justinmari@gmail.com>
2024-01-27 00:36:51 -0700

    fix bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
-import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
