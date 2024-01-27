0290a89c7cf56bf7c58f6232e2771602729f5961
jmgarcia0310 <garcia.justinmari@gmail.com>
2024-01-27 00:38:27 -0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Router>
         <Routes>
           <Route path="/ingraph-e-commerce/" element={<Home />} />
-          <Route path="/ingraph-e-commerce/signin" element={<SignIn />} />
           <Route path="/ingraph-e-commerce/signup" element={<SignUp />} />
         </Routes>
       </Router>
